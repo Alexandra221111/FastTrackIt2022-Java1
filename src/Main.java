f4c4a75277d2798c352ef0ae2bdc404896b5117e
Alexandra Siminciuc <siminciuc2000@gmail.com>
2022-10-12 20:35:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello");
+    System.out.println("Alexandra");
+
+    byte result = 60 + 30;
+    System.out.println(result);
+
+    byte resultOne = 87 / 2;
+    System.out.println(resultOne);
+
+    byte resultTwo = -5 + (8 * 6);
+    System.out.println(resultTwo);
+
+    byte resultThree = (55+9) % 9;
+    System.out.println(resultThree);
+
+    byte resultFour = 20 + (-3*5 )/ 8;
+    System.out.println(resultFour);
+
+    byte resultFive = 5 + 15 / (3 * 2) - (8 % 3);
+    System.out.println(resultFive);
+
+
+    }
+}
